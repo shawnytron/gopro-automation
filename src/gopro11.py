69c50eebb2f3b7b240c1824ce9cda9da88e92c3d
shawnyang <z63yang@uwaterloo.ca>
2024-09-22 19:52:20 -0400

    update code

--- src/gopro11.py
+++ src/gopro11.py
@@ -1,3 +1,13 @@
 import asyncio
 from open_gopro import WirelessGoPro
 
+
+async def main() -> None:
+    async with WirelessGoPro() as gopro:
+        print("Yay! I'm connected via BLE, Wifi, opened, and ready to send / get data now!")
+        # Send some messages now
+
+    
+        
+if __name__ == "__main__":
+    asyncio.run(main())
